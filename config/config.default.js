8e1ebed1e41bfe12b76f16af760e061d509b2d30
SeaChan <1341914555@qq.com>
2021-10-28 14:53:47 +0800

    fix: 修改MongoDB 连接url 为 127.0.0.1

--- config/config.default.js
+++ config/config.default.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dbUri: 'mongodb://127.0.0.1:27017/express-login',
+    dbUri: 'mongodb://127.0.0.1:27017/express-login', // admin 用户需在 url 后加 ?authSource=admin
     jwtSecret: 'C217C3B7-0FD2-5F3B-EA84-6F2CCC45BA33',
     mongooseOptions: {
         useNewUrlParser: true,
